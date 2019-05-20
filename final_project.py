031e59089613fe836a3fdbf63edaa44646cd4ae5
Cormac <cormacdoyle802@gmail.com>
2019-05-20 14:13:59 -0400

    drawing rectangle no movement

--- final_project.py
+++ final_project.py
@@ -5,8 +5,26 @@ import arcade
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 BACKGROUND_COLOUR = arcade.color.WHITE
+SPRITE_COLOUR = arcade.color.ARTICHOKE
 
 # creating game class
+
+class Player:
+    def __init__(self, center_x, center_y, change_x, change_y):
+        self.center_x = center_x
+        self.center_y = center_y
+        self.change_x = change_x
+        self.change_y = change_y
+        self.player_sprite_list = arcade.SpriteList()
+
+    def draw(self):
+        size = 50
+        arcade.draw_rectangle_filled(self.center_x, self.center_y, size, size, SPRITE_COLOUR)
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
 
 class MyGame(arcade.Window):
 
@@ -16,7 +34,16 @@ class MyGame(arcade.Window):
 
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Our game name TBD")
 
+        self.player = Player(200, 200, 0, 0)
+
+        self.player_sprite = None
+        self.player_sprite_list = None
         arcade.set_background_color(BACKGROUND_COLOUR)
+
+
+    # defining setup function
+
+    # def setup(self):
 
 
     # defining drawing function
@@ -24,10 +51,13 @@ class MyGame(arcade.Window):
     def on_draw(self):
         arcade.start_render()
 
+        self.player.draw()
+
 
     # defining update function
 
-
+    def update(self, delta_time):
+        self.player.update()
 
 # defining main function
 
